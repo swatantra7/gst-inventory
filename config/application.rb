039fdc48f345a05dd2114f163413ca5410926b6a
swatantra7 <swatantrajha7@gmail.com>
2017-09-17 13:05:28 +0530

    production fixes

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module GstInventory
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += %w(*.js)
-    config.public_file_server.enabled = true
 
   end
 end
