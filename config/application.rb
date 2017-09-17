41f646716a8fb41b4ebd48b95bcf839cde5543ad
swatantra7 <swatantrajha7@gmail.com>
2017-09-17 12:30:26 +0530

    This commit refs - Fixes for production

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module GstInventory
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += %w(*.js)
+    config.serve_static_assets = true
+
   end
 end
