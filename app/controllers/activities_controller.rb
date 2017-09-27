ea09f4607fef046e84d49c904afbb167d989495b
swatantra7 <swatantrajha7@gmail.com>
2017-09-28 00:38:43 +0530

    this commit refs bug- current user issue with public activity and serach items

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,9 @@
 class ActivitiesController < ApplicationController
 
   def index
-  	@activities = PublicActivity::Activity.order("created_at desc")
+    @activities = PublicActivity::Activity
+      .where(owner_id: current_user.id, owner_type: 'User')
+      .order("created_at desc")
   end
 
 end
