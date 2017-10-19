7d07d79d592dc2c7ffeebba57ea00de47eb050c9
swatantra7 <swatantrajha7@gmail.com>
2017-10-19 17:56:41 +0530

    This commit refs - removal of signup functaionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :valid_request
+
+
+  private
+
+  def valid_request
+    if unpermitted_request
+      redirect_to new_user_session_path
+    end
+  end
+
+  def unpermitted_request
+    puts controller_name
+    controller_name ==  'registrations'
+  end
+
 end
